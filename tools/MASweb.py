7d7f990da176cc021e961a93255a130312e90461
opalissan <oissan@predsci.com>
2021-04-21 21:02:55 -0700

    OI - Update by automatically grabbing data from psi website. Add figures for all 7 PSP perihelion cr intervals.

---
+++ tools/MASweb.py
@@ -0,0 +1,33 @@
+""" Read in MAS results from PSI (Predictive Science inc.) website"""
+from pathlib import Path
+from parfive import Downloader
+
+
+def get_mas_path(cr):
+    """Get MAS website results.
+
+    :param cr: carrington rotation. ex: 2210
+    :return: mas_path
+    """
+    download_dir = Path.cwd() / '..' / '..' / 'data'
+    
+    cr_string = 'mas_helio/cr' + str(cr)
+    mas_helio_dir = download_dir / cr_string 
+    mas_helio_dir.mkdir(parents=True, exist_ok=True)
+    base_url = 'http://www.predsci.com/data/runs/cr' + str(cr) + '-medium/hmi_mast_mas_std_0201/helio/{var}002.hdf'
+
+    # Create a downloader to queue the files to be downloaded
+    dl = Downloader()
+
+    for var in ['rho', 'vr', 'br']:
+        file = mas_helio_dir / f'{var}002.hdf'
+        if file.exists():
+            continue
+        else:
+            remote_file = base_url.format(var=var)
+            dl.enqueue_file(remote_file, path=mas_helio_dir)
+
+    # Download the files
+    if dl.queued_downloads > 0:
+        dl.download()
+    return mas_helio_dir.resolve()
